03abee3a569967e7049af2dddd6675ead38bca5a
hujiangpku <jianghu@pku.edu.cn>
2018-08-30 15:43:01 +0800

    update the retraction on the Stiefel manifold

--- manifolds-manopt/stiefelfactory.m
+++ manifolds-manopt/stiefelfactory.m
@@ -63,8 +63,12 @@ function M = stiefelfactory(n, p, k)
             t = 1.0;
         end
         Y = X + t*U;
-        for i = 1:k
-            Y(:,:,i) = Y(:,:,i)*(Y(:,:,i)'*Y(:,:,i))^(-0.5);
+        for i = 1 : k
+            [Q, R] = qr(Y(:, :, i), 0);
+            % The instruction with R assures we are not flipping signs
+            % of some columns, which should never happen in modern Matlab
+            % versions but may be an issue with older versions.
+            Y(:, :, i) = Q * diag(sign(sign(diag(R))+.5));
         end
 
     end
